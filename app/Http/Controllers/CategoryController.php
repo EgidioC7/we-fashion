b71ab843ce01b95be263bb785da4f9ee6c971073
Egidio CORICA <corica.egidio@gmail.com>
2019-06-28 13:10:51 +0200

    Add controller for category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public $paginate = 6;
+
+    public function index()
+    {
+        $categories = Category::paginate($this->paginate);
+
+        return view('back.category.index', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('back.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('category.index')->with('message', 'success');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        $category = Category::find($id);
+
+        return view('back.category.edit', ['category' => $category ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+        ]);
+
+        $category = Category::find($id);
+
+        $category->update($request->all());
+
+        return redirect()->route('category.index')->with('message', 'success');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        $category->delete();
+
+        return redirect()->route('category.index')->with('message', 'success');
+    }
+}
